5fa82e8e10f7ae5dc980f5b78d074e8dddcd4633
Johnnie Hsieh <taipai88@yahoo.com.tw>
2025-07-30 16:56:17 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -743,6 +743,13 @@ def delete_court(court_id):
     
     return redirect(url_for('admin_courts'))
 
+# if __name__ == '__main__':
+#     create_tables_and_initial_data() 
+#     app.run(debug=True)
+
+
 if __name__ == '__main__':
     create_tables_and_initial_data() 
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
